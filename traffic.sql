96ff0d73c6ba53aeb866372841c8bc2f8e92f325
Nitish Kumar <nitishmehta2021@gmail.com>
2022-10-18 22:41:15 +0530

    .

---
+++ traffic.sql
@@ -0,0 +1,29 @@
+create table traffic_police
+(name varchar(20), moblie_no number(11), licence_no varchar(20), vehicle_type varchar(8), vehicle_model varchar(28), area_assigned char(20), shift_duration varchar(20));
+insert into traffic_police(name, moblie_no, licence_no, vehicle_type, vehicle_model, area_assigned, shift_duration)
+
+values('shubhojeet', '8978992122', 'jh018786543322', '2wheeler', 'hero', 'kadru', 'morning');
+
+insert into traffic_police(name, moblie_no, licence_no, vehicle_type, vehicle_model, area_assigned, shift_duration)
+
+values('ramjeet', '8978992188', 'jh018786544456', '2wheeler', 'maruti', 'harmu', 'morning');
+
+insert into traffic_police(name, moblie_no, licence_no, vehicle_type, vehicle_model, area_assigned, shift_duration)
+
+values('ramgopal', '8978992123', 'jh018786566656', '2wheeler', 'maruti', 'harmu', 'afternoon'); 
+
+insert into traffic_police(name, moblie_no, licence_no, vehicle_type, vehicle_model, area_assigned, shift_duration)
+
+values('ranjeet', '7282992123', 'jh028786566656', '2wheeler', 'hero', 'kadru', 'afternoon');
+
+insert into traffic_police(name, moblie_no, licence_no, vehicle_type, vehicle_model, area_assigned, shift_duration)
+
+values('premdeep', '9282998143', 'jh028786655556', '2wheeler', 'hero', 'kishorganj', 'afternoon');
+
+insert into traffic_police(name, moblie_no, licence_no, vehicle_type, vehicle_model, area_assigned, shift_duration)
+
+values('sandeep', '8272998143', 'jh028786689766', '2wheeler', 'hero', 'ratu road', 'night');
+
+insert into traffic_police(name, moblie_no, licence_no, vehicle_type, vehicle_model, area_assigned, shift_duration)
+
+values('vivek', '9504998143', 'jh028741669766', '2wheeler', 'hero', 'doranda', 'night');
